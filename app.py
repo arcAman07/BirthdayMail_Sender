b71c8889eea2a5c7abd8a4f8b1504fb26c5886ff
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-06 17:53:30 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,4 +2,8 @@
 import pandas as pd
 import numpy as np
 data = pd.read_csv("C:/Users/amans/Downloads/mailingList.csv")
-print(data.head())
+Emails = []
+Names = []
+for i in range(len(data)):
+    Emails.append(data.iloc[i,2])
+    Names.append(data.iloc[i,1])
