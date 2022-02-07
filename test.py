6f810499053e423fb866a0f7d3b1f3ef4f4a1ac8
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-07 12:33:22 +0530

    Initial commit

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 import csv
-
+from csv import writer
 # open the file in the write mode
 f = open('C:/Users/amans/Downloads/mailingList.csv', 'w')
 
 # create the csv writer
-spamwriter = csv.writer(f)
+# spamwriter = writer(f)
 
 row = [4,"Tanmay","b@gmail.com"]
 
-# write a row to the csv file
-spamwriter.writerow(row)
+# # write a row to the csv file
+# spamwriter.writerow(row)
 
-# close the file
-f.close()
+# # close the file
+# f.close()
+
+with open(f, 'a') as f_object:
+  
+    # Pass this file object to csv.writer()
+    # and get a writer object
+    writer_object = writer(f_object)
+  
+    # Pass the list as an argument into
+    # the writerow()
+    writer_object.writerow(row)
+  
+    #Close the file object
+    f_object.close()
