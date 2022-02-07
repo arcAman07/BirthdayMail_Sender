245fc3c08284ff36b82f4c9bf7670a45bf36223c
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-07 11:33:32 +0530

    Initial commit

--- database.py
+++ database.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import pyodbc
-
+import mysql.connector
 # Import CSV
 data = pd.read_csv("C:/Users/amans/Downloads/mailingList.csv")
 df = pd.DataFrame(data)
 
 # Connect to SQL Server
-conn = pyodbc.connect(
-    "Driver={SQL Server};"
-    "Server=localhost:3306;"
-    "Database=node-reference;"
-    "Trusted_Connection=yes;"
-)
+cnxn = pyodbc.connect(driver='{SQL Server}', host="localhost", database="node-reference",
+                      trusted_connection="yes", user="root", password="Aman0712")
 cursor = conn.cursor()
 
 # Create Table
